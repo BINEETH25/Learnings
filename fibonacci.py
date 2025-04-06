edb032a2ea1524e8d6b325c794cfb5bb37866d7c
BINEETH25 <biineethreddy25@gmail.com>
2025-04-06 17:23:09 -0400

    2. fibonacci with recursion

--- fibonacci.py
+++ fibonacci.py
@@ -4,13 +4,29 @@ cur = 1
 print(prev)
 print(cur)
 
+''' fibonacci using for loop
 for i in range(10):
     new = prev + cur
     print(new)
     prev = cur
     cur = new
     
+'''
 
+count = 2
 
+def fibonacci(previous, current):
+    global count
+    if count <= 10:
+        newf = previous + current
+        print(newf)
+        previous = current
+        current = newf
+        count += 1
+        fibonacci(previous, current)
+    else:
+        return
+
+fibonacci(0, 1)
 
 
