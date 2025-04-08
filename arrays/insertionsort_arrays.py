91697212df09aa089d3c24cd2075dbef82638f32
BINEETH25 <biineethreddy25@gmail.com>
2025-04-08 15:22:52 -0400

    2.Optimized

--- arrays/insertionsort_arrays.py
+++ arrays/insertionsort_arrays.py
@@ -3,10 +3,16 @@ my_array = [25, 28, 10, 16, 17, 38, 24, 6, 29, 41]
 n = len(my_array)
 for i in range(1,n):
     insert_index = i
-    cur_value = my_array.pop(i)
+    #cur_value = my_array.pop(i)
+    cur_value = my_array[i]
     for j in range(i-1, -1, -1):
         if my_array[j] > cur_value:
+            # optimizing by swapping the value from sorted value with index
+            my_array[j + 1] = my_array[j]
             insert_index = j
-    my_array.insert(insert_index, cur_value)
+        else:
+            break
+    #my_array.insert(insert_index, cur_value)
+    my_array[insert_index] = cur_value
 
 print(my_array)
