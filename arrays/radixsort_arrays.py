466e38c24330606eaacf8c5166aabdb29ae9e7b8
BINEETH25 <biineethreddy25@gmail.com>
2025-04-10 01:00:00 -0400

    Radix sort with bubble sort implementation

--- arrays/radixsort_arrays.py
+++ arrays/radixsort_arrays.py
@@ -1,3 +1,4 @@
+
 myArray = [170, 45, 75, 90, 802, 24, 2, 66]
 print("Original array:", myArray)
 
@@ -20,3 +21,42 @@ while max_val // exp > 0:
     exp *= 10
     
 print(myArray)
+
+''' 
+#Radix Sort With Bubble Sort Sorting Implementation
+
+def bubbleSort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                
+def radixSortWithBubbleSort(arr):
+    max_val = max(arr)
+    exp = 1
+    
+    while max_val // exp > 0:
+        radixArray = [[],[],[],[],[],[],[],[],[],[]]
+        
+        for num in arr:
+            radixIndex = (num // exp) % 10
+            radixArray[radixIndex].append(num)
+        
+        for bucket in radixArray:
+            if len(bucket) > 1:
+                bubbleSort(bucket)
+        
+        i = 0
+        for bucket in radixArray:
+            for num in bucket:
+                arr[i] = num
+                i += 1
+        
+        exp *= 10
+
+myArray = [170, 45, 75, 90, 802, 24, 2, 66]
+print("Original array:", myArray)
+radixSortWithBubbleSort(myArray)
+print("Sorted array:", myArray)
+'''
