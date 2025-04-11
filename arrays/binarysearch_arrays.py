864c32bba34ad6b091efadea29b022f7cfc3ede1
BINEETH25 <biineethreddy25@gmail.com>
2025-04-10 21:03:03 -0400

    binary search

---
+++ arrays/binarysearch_arrays.py
@@ -0,0 +1,26 @@
+def BinarySearch(arr, targetVal):
+    left = 0
+    right = len(arr) - 1
+    
+    while left < right:
+        mid = (left + right) // 2
+        
+        if arr[mid] == targetVal:
+            return mid
+        
+        if arr[mid] < targetVal:
+            left = mid + 1
+        else:
+            right = mid - 1
+            
+    return -1
+
+myArray = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+myTarget = 15
+
+result = BinarySearch(myArray, myTarget)
+
+if result != -1:
+    print("Value", myTarget, "found at index : ", result)
+else:
+    print("Value", myTarget, "not found in array")
