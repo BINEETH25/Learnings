b7104a8f7389172931ee710ad05962a311a75cc2
Bineeth Reddy Narra <bineethreddynarra@Bineeths-MacBook-Air.local>
2025-06-13 19:20:04 -0400

    2.

--- Practice/fibonnacci.py
+++ Practice/fibonnacci.py
@@ -1,3 +1,4 @@
+print("---Fibonacci---")
 prev2 = 0
 prev1 = 1
 num = 10
@@ -18,3 +19,13 @@ def F(n):
         return F(n - 1) + F(n - 2)
 
 print(F(10))
+print("---Finding Lowest value in array---")
+# Finding Lowest value in array
+array = [12, 16, 25, 28, 2, 10, 6]
+lowest_num = array[0]
+for i in range(len(array)):
+    if array[i] < lowest_num:
+        lowest_num = array[i]
+
+print(lowest_num)
+print("--Bubble Sort--")
