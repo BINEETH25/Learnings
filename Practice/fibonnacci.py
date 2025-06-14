d1c318562e37ab91082edf5af5b53dae89695416
Bineeth Reddy Narra <bineethreddynarra@Bineeths-MacBook-Air.local>
2025-06-14 10:51:02 -0400

    Insertion Sort

--- Practice/fibonnacci.py
+++ Practice/fibonnacci.py
@@ -42,7 +42,7 @@ for i in range(n-1):
     if not Swapped:
         break
 print(array1)
-'''
+
 print("----Selection SOrt----")
 array2 = [12, 16, 25, 28, 2, 10, 6]
 n = len(array2)
@@ -53,3 +53,18 @@ for i in range(n-1):
             min_index = j
     array2[i], array2[min_index] = array2[min_index], array2[i]
 print(array2)
+'''
+print("------Insertion Sort------")
+array3 = [12, 16, 25, 28, 2, 10, 6]
+n = len(array3)
+for i in range(1, n):
+    insert_index = i
+    curr_value = array3[i]
+    for j in range(i-1, -1, -1):
+        if array3[j] > curr_value:
+            array3[j+1] = array3[j]
+            insert_index = j
+        else:
+            break
+    array3[insert_index] = curr_value
+print(array3)
