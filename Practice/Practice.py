4cb04407c920445276e7d370d13dca1bbba24b0f
Bineeth Reddy Narra <bineethreddynarra@Bineeths-MacBook-Air.local>
2025-06-14 21:20:48 -0400

    Practice2.

--- Practice/Practice.py
+++ Practice/Practice.py
@@ -1,3 +1,4 @@
+'''
 # Bubble Sort --> Adaptive, Stable.
 array = [8, 8, 7, 3, 2]
 n = len(array)
@@ -13,3 +14,15 @@ print(array)
 # Time Complexity :
 #       Best Case : O(n)
 #       Worst Case: O(n^2)
+'''
+# Selection Sort : repeatedly finding the minimum element from the unsorted part to moving it to the sorted part.
+array = [8, 6, 7, 3, 2, 25, 16, 32, 1]
+n = len(array)
+for i in range(n):
+    min_index = i
+    for j in range(i+1, n):
+        if array[j] < array[min_index]:
+            min_index = j
+    array[i], array[min_index] = array[min_index], array[i]
+print(array)
+# Time Complexity : O(n^2)
